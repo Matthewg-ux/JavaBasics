a50c53842cfc44de022040032908ddc15c138925
matthew gallati <matthewgallati@students.berkeley.net>
2020-02-06 13:18:25 -0800

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        Car myCar = new Car(2001, "toyota", 70000,  "prius");
+        System.out.println(myCar.toString());
+        Rectangle myRectangle = new Rectangle(50,40);
+        System.out.println(myRectangle.toString());
+    }
+}
